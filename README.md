c38e5bb3bf6179f5b89ceb455116af1db5820c24
Konstantin Bochkarev <bochkarev@haulmont.com>
2021-10-11 13:56:22 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Docker run:
+
+docker run --rm -p 8080:8080 docker.io/library/arc015:0.0.1-SNAPSHOT
---
+++ src/test/java/su/ermine/arc015/Arc015ApplicationTests.java
@@ -0,0 +1,13 @@
+package su.ermine.arc015;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arc015ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
